56475c34b037147dd94860a7bd2a322adc3564c3
Pritesh Gupta <priteshjgupta@gmail.com>
2025-05-02 21:20:24 +0530

    Create degree_list.py

---
+++ kanaka_parser/degree_list.py
@@ -0,0 +1,20 @@
+education_keywords = [
+    # Bachelor's Degrees
+    "B.E", "B.Tech", "Bachelor of Engineering", "Bachelor of Technology",
+    "BSc", "BS", "Bachelor of Science",
+    
+    # Master's Degrees
+    "M.E", "M.Tech", "Master of Engineering", "Master of Technology",
+    "MSc", "MS", "Master of Science",
+    "MCA", "Master of Computer Applications",
+    "MBA", "Master of Business Administration",
+    
+    # Doctorate
+    "PhD", "Doctor of Philosophy",
+
+    # Diploma
+    "Diploma",
+
+    # Generic forms
+    "Bachelor", "Master", "Graduate", "Postgraduate"
+]
